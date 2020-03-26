e6ef7bb87ca2eb28f019eba0b2c208030fb892ef
Dmitriy Khvan <dmitriy.khvan@Dmitriys-MacBook-Pro.local>
2020-03-26 03:07:28 +0100

    18747

--- Source/Unification/unification.cpp
+++ Source/Unification/unification.cpp
@@ -794,7 +794,8 @@ void ToneUnifier::applyColorRemappingforImages(bool needIndividuals, bool applyR
 	Mat baseImage(rows, cols, CV_8UC3, Scalar(BKGRNDPIX, BKGRNDPIX, BKGRNDPIX));
 	uchar *basePtr = (uchar*)baseImage.data;
 	//! warp images in the order of their ID, namely imgNo
-	vector<int> indices = {0,2,1};
+	// vector<int> indices = {0,2,1};
+	vector<int> indices = {0,1};
 
 	for (int i = 0; i < _imgNum; i ++)
 	{
